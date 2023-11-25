9c03c224da647e72493ebfbc327d912a87e3838a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-25 13:26:55 +0100

    Implement findAll method

--- ehcache-java-demo/src/main/java/ehcache/java/demo/person/dao/PersonDaoImpl.java
+++ ehcache-java-demo/src/main/java/ehcache/java/demo/person/dao/PersonDaoImpl.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,25 +22,37 @@ public class PersonDaoImpl implements Dao<Person> {
     public Person findById(Long id) {
         try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM person WHERE id = ?")) {
             statement.setLong(1, id);
-            try (ResultSet rs = statement.executeQuery()) {
-                if (rs.next()) {
-                    String firstName = rs.getString("first_name");
-                    String lastName = rs.getString("last_name");
-                    Integer age = rs.getInt("age");
-                    return new Person(id, firstName, lastName, age);
-                }
-            } catch (SQLException e) {
-                log.error("Database access error occurred.");
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                Long actualId = rs.getLong("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                Integer age = rs.getInt("age");
+                return new Person(actualId, firstName, lastName, age);
             }
         } catch (SQLException e) {
-            log.error("Database access error occurred.");
+            throw new RuntimeException(e);
         }
         return null;
     }
 
     @Override
     public List<Person> findAll() {
-        return null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM person")) {
+            ResultSet rs = statement.executeQuery();
+            List<Person> people = new ArrayList<>();
+            while (rs.next()) {
+                Long id = rs.getLong("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                Integer age = rs.getInt("age");
+                people.add(new Person(id, firstName, lastName, age));
+            }
+            log.info("Retrieved {} people from database.", people.size());
+            return people;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
