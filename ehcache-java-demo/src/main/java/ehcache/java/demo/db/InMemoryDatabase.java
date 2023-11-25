d3cb7d994b8cb581240c59418d81c0b0f938b94c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-25 13:18:55 +0100

    Enable connection object acquiring

--- ehcache-java-demo/src/main/java/ehcache/java/demo/db/InMemoryDatabase.java
+++ ehcache-java-demo/src/main/java/ehcache/java/demo/db/InMemoryDatabase.java
@@ -1,6 +1,7 @@
 package ehcache.java.demo.db;
 
 import ehcache.java.demo.db.loading.DataLoader;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
@@ -8,6 +9,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @Slf4j
+@Getter
 public class InMemoryDatabase {
 
     private static final String H2_URL = "jdbc:h2:mem:test";
