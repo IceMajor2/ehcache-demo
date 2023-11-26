0b4782c32f470945192131af4c8a99e6f0bbe469
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-26 19:38:21 +0100

    Use different command to insert test data

---
+++ ehcache-java-demo/src/test/resources/data.sql
@@ -0,0 +1,7 @@
+MERGE INTO person (ID, FIRST_NAME, LAST_NAME, AGE)
+     VALUES (1, 'John', 'Kowalski', 44),
+            (2, 'Guybrush', 'Threepwood', 25),
+            (3, 'Toby', 'Randall', 31),
+            (4, 'Tina', 'Swain', 28),
+            (5, 'Marco', 'Lister', 81),
+            (6, 'Robert', 'Koch', 80);
