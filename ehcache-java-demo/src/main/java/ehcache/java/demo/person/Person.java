b74e609973ad9a587dc78e313b678b49a9beae9c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-26 19:42:20 +0100

    Implement Serializable for Ehcache

--- ehcache-java-demo/src/main/java/ehcache/java/demo/person/Person.java
+++ ehcache-java-demo/src/main/java/ehcache/java/demo/person/Person.java
@@ -1,11 +1,13 @@
 package ehcache.java.demo.person;
 
+import java.io.Serializable;
+
 public record Person(
         Long id,
         String firstName,
         String lastName,
         Integer age
-) {
+) implements Serializable {
 
     public Person(Long id, String firstName, String lastName, Integer age) {
         this.id = id;
