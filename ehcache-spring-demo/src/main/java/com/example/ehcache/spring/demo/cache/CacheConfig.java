16987fce090ee2ada44ceb485ee7b62e2d314ff7
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-27 19:50:47 +0100

    Configure cache manager

--- ehcache-spring-demo/src/main/java/com/example/ehcache/spring/demo/cache/CacheConfig.java
+++ ehcache-spring-demo/src/main/java/com/example/ehcache/spring/demo/cache/CacheConfig.java
@@ -1,9 +1,40 @@
 package com.example.ehcache.spring.demo.cache;
 
+import com.example.ehcache.spring.demo.person.Person;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.jsr107.Eh107Configuration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public CacheManager ehcacheManager() {
+        CacheConfiguration<Long, Person> config = CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(Long.class, Person.class,
+                        ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                .offheap(10, MemoryUnit.MB)
+                                .build())
+                .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofHours(1)))
+                .build();
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        javax.cache.configuration.Configuration<Long, Person> configuration = Eh107Configuration
+                .fromEhcacheCacheConfiguration(config);
+        cacheManager.createCache("personCache", configuration);
+        return cacheManager;
+    }
 }
