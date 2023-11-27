ab962757723168b3e1d0098d6aca16bcf5add72e
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-27 18:38:18 +0100

    Enable caching

---
+++ ehcache-spring-demo/src/main/java/com/example/ehcache/spring/demo/cache/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.example.ehcache.spring.demo.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
