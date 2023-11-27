471dafe60e4302bd94955b80f83ab1c119634530
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-27 18:22:36 +0100

    Add Person domain model

---
+++ ehcache-spring-demo/src/main/java/com/example/ehcache/spring/demo/person/Person.java
@@ -0,0 +1,22 @@
+package com.example.ehcache.spring.demo.person;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.io.Serializable;
+
+@Table("person")
+public record Person(
+        @Id Long id,
+        String firstName,
+        String lastName,
+        Integer age
+) implements Serializable {
+
+    public Person(Long id, String firstName, String lastName, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
